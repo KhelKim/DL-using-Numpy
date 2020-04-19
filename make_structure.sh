0d15b2c8cc7f5e51fecc2cf2e41a5b147aeef1d2
Khel Kim <hyunho.kim@platfarm.net>
2020-04-19 19:46:08 +0900

    chatper1 구현

--- make_structure.sh
+++ make_structure.sh
@@ -2,6 +2,6 @@
 
 # sh make_structure.sh [만들폴더 이름]
 mkdir $1
-mkdir $1/implementation
+mkdir $1/codes
 touch $1/README.md $1/set_data.sh
-touch $1/implementation/implement.py $1/run.py
+touch $1/codes/implement.py $1/codes/run.py
